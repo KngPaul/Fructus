0145993de99e46e736870fe01397f7be0c5c1180
Paul Onawola <onawola.ire@gmail.com>
2024-05-18 13:30:44 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Paul Onawola on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
